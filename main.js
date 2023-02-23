71e812475ef6fe28e3d3c5f332516ffc817efc62
Drey766 <andrewkimani766@gmail.com>
2023-02-24 02:14:32 +0300

    Added files

---
+++ main.js
@@ -0,0 +1,13 @@
+$("#search-icon").click(function() {
+    $(".nav").toggleClass("search");
+    $(".nav").toggleClass("no-search");
+    $(".search-input").toggleClass("search-active");
+    });
+
+$('.menu-toggle').click(function(){
+    $(".nav").toggleClass("mobile-nav");
+    $(this).toggleClass("is-active");
+});
+
+
+
